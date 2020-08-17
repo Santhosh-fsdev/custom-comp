4312de271a69d08c38cd33500f8edaaedc8740e9
Santhosh Kumar Rajendran <santhoshfsdev@gmail.com>
2020-08-16 20:16:47 -0400

    Updates

---
+++ precache-manifest.6f89077b2b117c8fb358c691e86ceadd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a330188d3b1d21cdba8edd6ebb370e73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "159ff30b2f1186e9c4a7",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "5259be4e18414ae4d9a4",
+    "url": "./static/js/2.0f809715.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f809715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159ff30b2f1186e9c4a7",
+    "url": "./static/js/main.3cef6369.chunk.js"
+  },
+  {
+    "revision": "6bb7f74a2cf6b91ee805",
+    "url": "./static/js/runtime-main.bc75d18c.js"
+  }
+]);
